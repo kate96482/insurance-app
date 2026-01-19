1d420d11ac8f3b6dc9f3c9d3b50aa3064512d9f9
kate96482 <chen0850813@gmail.com>
2026-01-19 10:23:17 +0800

    initial commit

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'insurance-tools-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'https://cdn.jsdelivr.net/npm/chart.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
